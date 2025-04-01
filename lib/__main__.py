d948a6df5f13f822663d7480006faabebd667d13
sdemiriz <sedatdemiriz97@gmail.com>
2025-04-01 19:28:41 -0400

    add: manual testing of basic funtionality

--- lib/__main__.py
+++ lib/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from lib import SubsampleBAM
+from lib import SubsampleBAM, BAMloader
 import argparse
 
 
@@ -18,6 +18,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print(args)
+    bam = BAMloader.BAMloader(in_file=args.in_bam)
+    [a for a in bam.get_reads(6, 25_000_000, 35_000_000)]
+
+    bam.plot_pileup(contig=6, start=25_000_000, end=35_000_000)
     # test = SubsampleBAM(
     #     in_file=args.i,
     #     out_file=args.o,
