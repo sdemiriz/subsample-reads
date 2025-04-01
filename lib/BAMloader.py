5933518570ada9d1a11ae48d7c32e2edae76e99b
sdemiriz <sedatdemiriz97@gmail.com>
2025-04-01 19:30:04 -0400

    feat: add read fetching and pileup plotting functionality

--- lib/BAMloader.py
+++ lib/BAMloader.py
@@ -1,17 +1,43 @@
 import pysam
+import matplotlib.pyplot as plt
 
 
 class BAMloader:
 
-    def __init__(self, in_file: str, c: str, start: int, end: int):
+    def __init__(self, in_file: str):
 
         self.in_file = in_file
-        self.chr = c
-        self.start = start
-        self.end = end
 
         self.load_bam()
-        self.reads = self.bam.fetch(contig=self.chr, start=self.start, end=self.end)
 
     def load_bam(self):
         self.bam = pysam.AlignmentFile(self.in_file, "rb")
+
+    def get_reads(self, contig: int, start: int, end: int):
+        return self.bam.fetch(contig=str(contig), start=start, end=end)
+
+    def plot_pileup(
+        self, contig: int, start: int, end: int, out: str = "bamloader-plot.png"
+    ):
+        contig = str(contig)
+
+        pileup = [
+            (p.pos, p.n) for p in self.bam.pileup(contig=contig, start=start, end=end)
+        ]
+
+        fig, ax = plt.subplots(layout="constrained")
+
+        ax.plot(
+            [p[0] for p in pileup],
+            [p[1] for p in pileup],
+            label=f"{self.in_file.split('/')[-1]}",
+        )
+
+        ax.grid(visible=True, linestyle="--", linewidth=1)
+        ax.ticklabel_format(useOffset=False, style="plain")
+        ax.set_title(f"Coverage across chr{contig}:{start}-{end}")
+        ax.set_xlabel("Chromosomal coordinate")
+        ax.set_ylabel("Depth of coverage")
+        ax.legend()
+
+        plt.savefig(out)
