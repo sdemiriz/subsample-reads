72e40dcc6625c00e06a1dd3e93429b1b44d4f7dd
sdemiriz <sedatdemiriz97@gmail.com>
2025-04-07 09:49:07 -0400

    feat: update main file with new class methods

--- lib/__main__.py
+++ lib/__main__.py
@@ -1,27 +1,46 @@
 #!/usr/bin/env python
 
-from lib import SubsampleBAM, BAMloader
-import argparse
+from lib.BEDloader import BEDloader
+from lib.BAMloader import BAMloader
+import argparse, math
+import numpy as np
 
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(
         prog="subsample-reads",
         description="Tool to regionally downsample reads in BAM files",
     )
 
-    parser.add_argument("-i", "--in-bam")
+    parser.add_argument("-i", "--in-bam", required=True)
+    parser.add_argument("-r", "--regions", required=True)
     parser.add_argument("-o", "--out-bam")
-    parser.add_argument("-r", "--regions")
-    parser.add_argument("-S", "--seed")
+    parser.add_argument("-S", "--seed", required=False, default=42)
 
     args = parser.parse_args()
 
-    print(args)
-    bam = BAMloader.BAMloader(in_file=args.in_bam)
-    [a for a in bam.get_reads(6, 25_000_000, 35_000_000)]
+    contig = 6
+    np.random.seed(args.seed)
 
-    bam.plot_pileup(contig=6, start=25_000_000, end=35_000_000)
+    bam = BAMloader(in_file=args.in_bam)
+    bed = BEDloader(
+        in_file=args.regions, chr_length=300_000
+    )  # , chr_length=bam.get_length(contig=contig))
+
+    for interval in bed.tree:
+
+        reads1, reads2 = bam.get_read_pairs(
+            contig=contig, start=interval.begin, end=interval.end
+        )
+
+        for read1, read2 in zip(reads1, reads2):
+            pass
+
+        # size = math.ceil(interval.data * sum(1 for r in reads))
+        # print(interval, sum(1 for r in reads))
+
+    # bam.plot_pileup(contig=6, start=25_000_000, end=35_000_000)
     # test = SubsampleBAM(
     #     in_file=args.i,
     #     out_file=args.o,
