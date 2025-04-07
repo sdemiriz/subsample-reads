43694f070eaa02b6438d4b126e3f3c3c2a092ae8
sdemiriz <sedatdemiriz97@gmail.com>
2025-04-07 09:15:46 -0400

    feat: paired read fetching from BAM

--- lib/BAMloader.py
+++ lib/BAMloader.py
@@ -1,5 +1,6 @@
 import pysam
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 
 class BAMloader:
@@ -7,14 +8,39 @@ class BAMloader:
     def __init__(self, in_file: str):
 
         self.in_file = in_file
-
         self.load_bam()
+
+        self.read_dict = defaultdict(lambda: [None, None])
 
     def load_bam(self):
         self.bam = pysam.AlignmentFile(self.in_file, "rb")
 
-    def get_reads(self, contig: int, start: int, end: int):
-        return self.bam.fetch(contig=str(contig), start=start, end=end)
+    def get_length(self, contig: int):
+
+        contig = str(contig)
+        assert contig in self.bam.references, f"Given {contig=} not in BAM references"
+
+        return self.bam.lengths[self.bam.references.index(contig)]
+
+    def get_read_pairs(self, contig: int, start: int, end: int):
+
+        for read in self.bam.fetch(contig=str(contig), start=start, end=end):
+            if not read.is_proper_pair:
+                continue
+
+            qname = read.query_name
+            if qname not in self.read_dict:
+                if read.is_read1:
+                    self.read_dict[qname][0] = read
+                else:
+                    self.read_dict[qname][1] = read
+
+            else:
+                if read.is_read1:
+                    yield read, self.read_dict[qname][1]
+                else:
+                    yield self.read_dict[qname][0], read
+                del self.read_dict[qname]
 
     def plot_pileup(
         self, contig: int, start: int, end: int, out: str = "bamloader-plot.png"
