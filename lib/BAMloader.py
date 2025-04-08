0736ec3fd0368eb638d1ecb72fb304c0f859db37
sdemiriz <sedatdemiriz97@gmail.com>
2025-04-07 22:33:56 -0400

    feat: implement subsampling method

--- lib/BAMloader.py
+++ lib/BAMloader.py
@@ -1,19 +1,25 @@
-import pysam
+import pysam, math
 import matplotlib.pyplot as plt
+import numpy as np
 from collections import defaultdict
 
 
 class BAMloader:
 
-    def __init__(self, in_file: str):
+    def __init__(self, file: str, template=None):
 
-        self.in_file = in_file
-        self.load_bam()
+        self.file = file
+        self.load_bam(template=template)
 
         self.read_dict = defaultdict(lambda: [None, None])
+        self.dropped_read_pairs = list()
 
-    def load_bam(self):
-        self.bam = pysam.AlignmentFile(self.in_file, "rb")
+    def load_bam(self, template=None):
+
+        if template:
+            self.bam = pysam.AlignmentFile(self.file, mode="wb", template=template)
+        else:
+            self.bam = pysam.AlignmentFile(self.file, mode="rb")
 
     def get_length(self, contig: int):
 
@@ -29,18 +35,81 @@ class BAMloader:
                 continue
 
             qname = read.query_name
-            if qname not in self.read_dict:
+            if qname in self.read_dict:
+                if read.is_read1:
+                    yield read, self.read_dict[qname][1]
+                else:
+                    yield self.read_dict[qname][0], read
+
+            else:
                 if read.is_read1:
                     self.read_dict[qname][0] = read
                 else:
                     self.read_dict[qname][1] = read
 
-            else:
-                if read.is_read1:
-                    yield read, self.read_dict[qname][1]
-                else:
-                    yield self.read_dict[qname][0], read
-                del self.read_dict[qname]
+    def downsample_reads(
+        self,
+        out_bam,
+        contig: int,
+        start: int,
+        end: int,
+        fraction: float,
+    ):
+        # Get paired reads within the interval
+        all_reads_in_interval = list(
+            self.get_read_pairs(contig=str(contig), start=start, end=end)
+        )
+
+        # Get paired reads that have not been dropped before
+        predropped_reads = [
+            read_pair
+            for read_pair in all_reads_in_interval
+            if read_pair[0].query_name in self.dropped_read_pairs
+        ]
+
+        # Calculate how many paired reads need to be dropped
+        base_size = math.ceil((1.0 - fraction) * len(all_reads_in_interval))
+
+        # Get indices of reads that need to be dropped
+        remove_indices = np.random.choice(
+            a=len(all_reads_in_interval),
+            size=base_size - len(predropped_reads),
+            replace=False,
+        )
+
+        # Reads to add to out_bam
+        kept_reads = [
+            read_pair
+            for i, read_pair in enumerate(all_reads_in_interval)
+            if i not in remove_indices
+        ]
+
+        # Reads to remove
+        removed_reads = [
+            read_pair
+            for i, read_pair in enumerate(all_reads_in_interval)
+            if i in remove_indices
+        ]
+
+        removed_reads += predropped_reads
+
+        # Add removed reads to memory for future intervals
+        for removed_read_pair in removed_reads:
+            self.dropped_read_pairs.append(removed_read_pair[0].query_name)
+
+        # Write kept reads to out_bam
+        for read_pair in kept_reads:
+            out_bam.bam.write(read=read_pair[0])
+            out_bam.bam.write(read=read_pair[1])
+
+        print(
+            f"{str(contig)} {str(start)} {str(end)}, {len(all_reads_in_interval)=}, {len(removed_reads)=}, {len(self.dropped_read_pairs)=}"
+        )
+
+    def index(self):
+        sorted_file = f"{self.file.split('.')[0]}-sorted.{self.file.split('.')[1]}"
+        pysam.sort(self.file, "-o", sorted_file)
+        pysam.index(sorted_file)
 
     def plot_pileup(
         self, contig: int, start: int, end: int, out: str = "bamloader-plot.png"
@@ -56,7 +125,7 @@ class BAMloader:
         ax.plot(
             [p[0] for p in pileup],
             [p[1] for p in pileup],
-            label=f"{self.in_file.split('/')[-1]}",
+            label=f"{self.file.split('/')[-1]}",
         )
 
         ax.grid(visible=True, linestyle="--", linewidth=1)
--- lib/__main__.py
+++ lib/__main__.py
@@ -2,7 +2,7 @@
 
 from lib.BEDloader import BEDloader
 from lib.BAMloader import BAMloader
-import argparse, math
+import argparse
 import numpy as np
 
 
@@ -15,37 +15,28 @@ if __name__ == "__main__":
 
     parser.add_argument("-i", "--in-bam", required=True)
     parser.add_argument("-r", "--regions", required=True)
-    parser.add_argument("-o", "--out-bam")
+    parser.add_argument("-o", "--out-bam", default="out.bam")
     parser.add_argument("-S", "--seed", required=False, default=42)
 
     args = parser.parse_args()
 
-    contig = 6
     np.random.seed(args.seed)
 
-    bam = BAMloader(in_file=args.in_bam)
-    bed = BEDloader(
-        in_file=args.regions, chr_length=300_000
-    )  # , chr_length=bam.get_length(contig=contig))
+    in_bam = BAMloader(file=args.in_bam)
+    out_bam = BAMloader(file=args.out_bam, template=in_bam.bam)
+    bed = BEDloader(in_file=args.regions, chr_length=300_000)
+
+    # , chr_length=bam.get_length(contig=contig))
 
     for interval in bed.tree:
 
-        reads1, reads2 = bam.get_read_pairs(
-            contig=contig, start=interval.begin, end=interval.end
+        in_bam.downsample_reads(
+            out_bam=out_bam,
+            contig=bed.contig,
+            start=interval.begin,
+            end=interval.end,
+            fraction=interval.data,
         )
 
-        for read1, read2 in zip(reads1, reads2):
-            pass
-
-        # size = math.ceil(interval.data * sum(1 for r in reads))
-        # print(interval, sum(1 for r in reads))
-
-    # bam.plot_pileup(contig=6, start=25_000_000, end=35_000_000)
-    # test = SubsampleBAM(
-    #     in_file=args.i,
-    #     out_file=args.o,
-    #     coords=args.c,
-    #     seed=args.S,
-    #     limits=args.l,
-    # )
-    # test.run()
+    in_bam.bam.close()
+    out_bam.bam.close()
