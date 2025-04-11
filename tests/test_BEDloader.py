eb061264050593130038811e7cbfd13440b7ceec
sdemiriz <sedatdemiriz97@gmail.com>
2025-04-10 22:38:39 -0400

    test: add test for fraction outside 0-1 interval

--- tests/test_BEDloader.py
+++ tests/test_BEDloader.py
@@ -8,6 +8,10 @@ class TestBEDloader(unittest.TestCase):
         with self.assertRaises(Exception):
             BEDloader(in_file="tests/not-unique.bed")
 
+    def testFractionsOutsideInterval01(self):
+        with self.assertRaises(Exception):
+            BEDloader(in_file="tests/wrong-fractions.bed")
+
 
 if __name__ == "__main__":
     unittest.main()
