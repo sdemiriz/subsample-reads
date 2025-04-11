72e432326ac18d501486ea016b158ee33fd32081
sdemiriz <sedatdemiriz97@gmail.com>
2025-04-10 22:38:23 -0400

    test: add test to check for index creation

--- tests/test_BAMloader.py
+++ tests/test_BAMloader.py
@@ -2,23 +2,20 @@ import unittest, os
 from lib.BAMloader import BAMloader
 
 
-class TestSomething(unittest.TestCase):
+class TestBAMLoader(unittest.TestCase):
 
-    test_bam = "tests/HG002.hs37d5.30x.chr6.bam"
-    bam = BAMloader(in_file=test_bam)
+    test_bam_filename = "tests/HG002.hs37d5.30x.chr6.bam"
+    bam = BAMloader(file=test_bam_filename)
 
     def testContigNotInReferences(self):
-        with self.assertRaises(Exception):
 
-            self.bam.get_length(contig="NotGreatConfigName")
+        with self.assertRaises(Exception):
+            self.bam.get_length(contig="ImpossibleConfigName")
 
     def testIndexCreatedWhenNotPresent(self):
-        try:
-            self.bam.get_reads(contig=6, start=0, end=100_000)
-            os.remove(self.test_bam + ".bai")
 
-        except Exception:
-            self.fail("Index not created when it should have")
+        self.assertTrue(os.path.isfile(self.test_bam_filename + ".bai"))
+        os.remove(self.test_bam_filename + ".bai")
 
 
 if __name__ == "__main__":
