d4d5c9f23e8c4a1a124cecd0c841f12ab41ed5ee
sdemiriz <sedatdemiriz97@gmail.com>
2025-04-17 22:57:35 -0400

    feat: add subparser for managing multiple functions

--- lib/__main__.py
+++ lib/__main__.py
@@ -6,65 +6,93 @@ from lib.BAMplotter import BAMplotter
 import argparse, logging
 import numpy as np
 
-if __name__ == "__main__":
+logger = logging.getLogger("root")
 
-    logger = logging.getLogger("root")
+logging.basicConfig(
+    filename="log.txt",
+    level=logging.DEBUG,
+    format="{asctime} - [{levelname}]: {message}",
+    style="{",
+    datefmt="%H:%M:%S",
+)
 
-    logging.basicConfig(
-        filename="log.txt",
-        level=logging.DEBUG,
-        format="{asctime} - [{levelname}]: {message}",
-        style="{",
-        datefmt="%H:%M:%S",
-    )
-    logging.info("Begin log")
 
-    parser = argparse.ArgumentParser(
-        prog="subsample-reads",
-        description="Tool to regionally downsample reads in BAM files",
-    )
+def chart_mode(args):
+    """
+    Chart a distribution of reads from given BAM file
+    """
+    raise NotImplementedError()
 
-    parser.add_argument("-i", "--in-bam", required=True)
-    parser.add_argument("-r", "--regions", required=True)
-    parser.add_argument("-o", "--out-bam", default="out.bam")
-    parser.add_argument("-S", "--seed", required=False, default=42)
 
-    args = parser.parse_args()
-    logging.info(f"Accept arguments: {args}")
+def sample_mode(args):
+    """
+    Sample provided BAM file based on regions in BED file
+    """
+    in_bam = BAMloader(file=args.in_bam)
+    logging.info(f"Load input BAM file: {args.in_bam}")
 
-    # in_bam = BAMloader(file=args.in_bam)
-    # logging.info(f"Load input BAM file: {args.in_bam}")
-
-    # out_bam = BAMloader(file=args.out_bam, template=in_bam.bam)
-    # logging.info(f"Open output BAM file: {args.out_bam}")
+    out_bam = BAMloader(file=args.out_bam, template=in_bam.bam)
+    logging.info(f"Open output BAM file: {args.out_bam}")
 
     bed = Intervals(file=args.regions)
     logging.info(f"Load BED file: {args.regions}")
 
     # , chr_length=bam.get_length(contig=contig))
 
-    # logging.info(f"Start subsampling")
-    # for interval in bed.tree:
+    logging.info(f"Start sampling")
+    for interval in bed.tree:
 
-    #     logging.info(
-    #         f"Start subsample interval {bed.contig}:{interval.begin}-{interval.end}"
-    #     )
+        logging.info(
+            f"Start subsample interval {bed.contig}:{interval.begin}-{interval.end}"
+        )
 
-    #     in_bam.downsample_reads(
-    #         out_bam=out_bam,
-    #         contig=bed.contig,
-    #         start=interval.begin,
-    #         end=interval.end,
-    #         fraction=interval.data,
-    #         seed=args.seed,
-    #     )
+        in_bam.downsample_reads(
+            out_bam=out_bam,
+            contig=bed.contig,
+            start=interval.begin,
+            end=interval.end,
+            fraction=interval.data,
+            seed=args.seed,
+        )
 
-    #     logging.info(
-    #         f"End subsample interval {bed.contig}:{interval.begin}-{interval.end}"
-    #     )
+        logging.info(
+            f"End subsample interval {bed.contig}:{interval.begin}-{interval.end}"
+        )
 
-    # in_bam.close()
-    # out_bam.close()
+    in_bam.close()
+    out_bam.close()
+
+
+if __name__ == "__main__":
+
+    logging.info("Begin log")
+
+    parser = argparse.ArgumentParser(
+        prog="subsample-reads",
+        description="Regionally downsample reads in BAM files",
+    )
+
+    subparsers = parser.add_subparsers(
+        title="subcommands", description="valid subcommands", required=True
+    )
+
+    chart = subparsers.add_parser("chart")
+    chart.add_argument("-i", "--in-bam", required=True)
+    chart.add_argument("-w", "--window-size", default=1000000)
+    chart.add_argument("-r", "--regions", default="out.bed")
+    chart.set_defaults(func=chart_mode)
+
+    sample = subparsers.add_parser("sample")
+    sample.add_argument("-i", "--in-bam", required=True)
+    sample.add_argument("-r", "--regions", required=True)
+    sample.add_argument("-o", "--out-bam", default="out.bam")
+    sample.add_argument("-S", "--seed", required=False, default=42)
+    sample.set_defaults(func=sample_mode)
+
+    args = parser.parse_args()
+    logging.info(f"Accept arguments: {args}")
+
+    args.func(args)
 
     logging.info(f"End log\n")
 
