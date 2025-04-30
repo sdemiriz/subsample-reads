0794766c733c3dea4788bf91630c891006dbef13
sdemiriz <sedatdemiriz97@gmail.com>
2025-04-30 17:15:50 -0400

    test: overlap function

--- tests/test_BAMloader.py
+++ tests/test_BAMloader.py
@@ -12,6 +12,17 @@ class TestBAMLoader(unittest.TestCase):
         with self.assertRaises(Exception):
             self.bam.get_length(contig="ImpossibleConfigName")
 
+    def testOverlap(self):
+        self.assertTrue(self.bam.overlap((0, 10), (0, 10)))  # identity
+        self.assertFalse(self.bam.overlap((0, 10), (20, 30)))  # no overlap
+
+        self.assertTrue(self.bam.overlap((0, 5), (3, 7)))  # overlap with overhangs
+        self.assertTrue(self.bam.overlap((0, 10), (4, 5)))  # one inside other
+
+        self.assertTrue(self.bam.overlap((0, 10), (9, 20)))  # overlap
+        self.assertFalse(self.bam.overlap((0, 10), (10, 20)))  # X end Y start overlap
+        self.assertFalse(self.bam.overlap((0, 10), (11, 20)))  # sequential
+
 
 if __name__ == "__main__":
     unittest.main()
