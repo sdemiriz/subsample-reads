86b383a2a610b231e185a2f283d75305266cfc93
sdemiriz <sedatdemiriz97@gmail.com>
2025-04-30 17:17:44 -0400

    feat: assure IntervalTree is sorted when returned

--- subsample_reads/Intervals.py
+++ subsample_reads/Intervals.py
@@ -45,6 +45,8 @@ class Intervals:
             ), "BED file contains overlapping intervals"
             self.tree.add(Interval(begin=row[2], end=row[3], data=row[4]))
 
+        self.tree = IntervalTree(sorted(self.tree))
+
     def get_limits(self):
         """
         Return contig, min and max of the region described in BED file
