85385f5670b9f0386d7d6c7d54f707dd958209f4
sdemiriz <sedatdemiriz97@gmail.com>
2025-05-21 16:00:48 -0400

    feat: add read count barplots to output plot

--- subsample_reads/Plotter.py
+++ subsample_reads/Plotter.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from logging import info
 import pandas as pd
+import pysam
 
 
 class Plotter:
@@ -58,42 +59,78 @@ class Plotter:
         info("Plotter - Complete pileup BAMs")
         return pileups
 
+    def get_counts(self, start, end) -> list:
+        """
+        Pileup BAMs for the defined region
+        """
+        info("Plotter - Pileup BAMs")
+
+        bams = [
+            Loader(file=self.in_bam),
+            Loader(file=self.map_bam),
+            Loader(file=self.out_bam),
+        ]
+        counts = [
+            bam.bam.count(
+                contig=bam.normalize_contig(self.intervals.contig),
+                start=start,
+                end=end,
+            )
+            for bam in bams
+        ]
+
+        info("Plotter - Complete pileup BAMs")
+        return list(counts)
+
     def plot(self) -> None:
         """
         Plot provided BAM file pileups
         """
         info("Plotter - Begin plotting")
         fig, ax = plt.subplots(layout="constrained")
+        ax2 = ax.twinx()
 
         pileups = self.get_pileups()
 
         info("Plotter - Iterate pileups")
-        for p, b in zip(pileups, [self.in_bam, self.out_bam]):
+        for p, b, l in zip(
+            pileups, [self.in_bam, self.map_bam, self.out_bam], ["IN", "TARGET", "OUT"]
+        ):
 
             pileup = pd.DataFrame(
                 [(a.reference_pos, a.nsegments) for a in p], columns=["coord", "depth"]
             )
-
             ax.plot(
                 pileup["coord"],
                 pileup["depth"],
-                label=Path(b).stem,
-                alpha=0.5,
+                label=l,
+                alpha=0.8,
             )
-        info("Plotter - Complete iterate pileups")
 
+        info("Plotter - Draw boundaries")
         for b in self.boundaries:
             ax.axvline(x=b, linestyle="--", linewidth=1.5, color="red", alpha=0.3)
 
+        info("Plotter - Add fraction annotations")
         for row in self.intervals.bed.iterrows():
+            counts = self.get_counts(row[1]["start"], row[1]["end"])
             ax.text(
                 x=(row[1]["start"] + row[1]["end"]) / 2,
                 y=-10,
-                s=str(row[1]["read_count"])[:5],
+                s=str(row[1]["fraction"])[:5],
                 ha="center",
                 size="x-small",
             )
+            for i in range(3):
+                ax2.bar(
+                    x=(row[1]["start"] + row[1]["end"]) / 2
+                    - (2 - (i + 1)) * (row[1]["end"] - row[1]["start"]) / 3,
+                    height=counts[i],
+                    width=250,
+                    alpha=0.5,
+                )
 
+        info("Plotter - Add axes, title, legend")
         ax.ticklabel_format(useOffset=False, style="plain")
         ax.set_title(
             f"Coverage across {self.intervals.contig}:{self.intervals.start}-{self.intervals.end}"
