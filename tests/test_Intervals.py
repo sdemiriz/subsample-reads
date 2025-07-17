3c968e695fbc5acb160056795184389052a41a9f
sdemiriz <sedatdemiriz97@gmail.com>
2025-07-17 16:40:35 -0400

    feat: add Intervals unit tests

--- tests/test_Intervals.py
+++ tests/test_Intervals.py
@@ -1,36 +1,37 @@
 import unittest
+import os
+import pandas as pd
 from subsample_reads.Intervals import Intervals
 
 
 class TestIntervals(unittest.TestCase):
+    def test_init_valid_bed(self):
+        i = Intervals(bed_dir="tests", bed_file="tests/test-dataframe-dimensions.bed")
+        self.assertEqual(i.contig, "chr6")
+        self.assertEqual(i.start, 25000000)
+        self.assertEqual(i.end, 35000000)
+        self.assertEqual(len(i.tree), 10)
 
-    def testCorrectColumns(self):
-        i = Intervals(files=["tests/test-dataframe-dimensions.bed"])
-        for bed in i.beds:
-            self.assertListEqual(
-                ["contig", "start", "end", "read_count"], list(bed.columns)
-            )
+    def test_file_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            Intervals(bed_dir="tests", bed_file="tests/DOES_NOT_EXIST.bed")
 
-    def testCorrectDimensions(self):
-        i = Intervals(files=["tests/test-dataframe-dimensions.bed"])
-        for bed in i.beds:
-            self.assertTupleEqual((10, 4), bed.shape)
-
-    def testDifferentContigsInBED(self):
+    def test_non_unique_contigs(self):
         with self.assertRaises(AssertionError) as context:
-            Intervals(files=["tests/test-contigs-not-unique.bed"])
-
-        self.assertEqual(
-            str(context.exception), f"Not all contig values in BED file identical"
+            Intervals(bed_dir="tests", bed_file="tests/test-contigs-not-unique.bed")
+        self.assertIn(
+            "Not all contig values in BED file identical", str(context.exception)
         )
 
-    def testOverlappingIntervals(self):
+    def test_overlapping_intervals(self):
         with self.assertRaises(AssertionError) as context:
-            Intervals(files=["tests/test-overlapping-intervals.bed"])
+            Intervals(bed_dir="tests", bed_file="tests/test-overlapping-intervals.bed")
+        self.assertIn("BED file contains overlapping intervals", str(context.exception))
 
-        self.assertEqual(
-            str(context.exception), f"BED file contains overlapping intervals"
-        )
+    def test_get_bed_columns(self):
+        i = Intervals(bed_dir="tests", bed_file="tests/test-dataframe-dimensions.bed")
+        expected_cols = ["contig", "start", "end", "read_count", "fraction"]
+        self.assertListEqual(list(i.bed.columns), expected_cols)
 
 
 if __name__ == "__main__":
