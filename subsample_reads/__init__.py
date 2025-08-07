015e1fbfa988e2001d87d91e3db0fce39018a500
sdemiriz <sedatdemiriz97@gmail.com>
2025-08-07 13:32:08 -0400

    feat: add batching parameters

--- subsample_reads/__init__.py
+++ subsample_reads/__init__.py
@@ -1,3 +1,14 @@
+"""
+Subsample reads toolkit for BAM file processing
+"""
+
+# Performance configuration constants
+BATCH_SIZE = 1000  # Number of reads to process in batches
+WRITE_BATCH_SIZE = 10000  # Number of reads to write in batches
+CACHE_SIZE_LIMIT = 10000  # Maximum cache size before clearing
+CHUNK_SIZE = 1000000  # Size of genome chunks for parallel processing
+MAX_WORKERS = 8  # Maximum number of parallel workers
+
 from .Plotter import Plotter
 from .Loader import Loader
 from .Mapper import Mapper
